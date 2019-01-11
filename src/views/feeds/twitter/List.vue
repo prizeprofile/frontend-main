473511da13f564101acf11ff673b5251dce8be72
Michael Bausano <michael@joinpouch.com>
2019-01-11 13:29:33 +0000

    Quick fix

--- src/views/feeds/twitter/List.vue
+++ src/views/feeds/twitter/List.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="twitter-feed">
+    <info-brick
+      :data="{
+        title: '❗Important Notice for Twitter Competitions',
+        description: 'We have temporarily disabled one click entering for Twitter competitions as we were hitting Twitter limits caused by increasing number of our users. We are intensively working on a new system and you can expect it to go live very soon.<br><br>We are sorry for any inconvenience,<br>Michael and Jonny'
+      }"
+    ></info-brick>
+
     <competition
       v-for="competition in feed.content"
       :key="competition.id"
@@ -11,9 +18,10 @@
 <script>
 import { mapGetters } from 'vuex'
 import Competition from './Competition'
+import InfoBrick from '@/views/partials/common/InfoBrick'
 
 export default {
-  components: { Competition },
+  components: { Competition, InfoBrick },
 
   computed: {
     ...mapGetters(['feed'])
---
+++ src/views/partials/common/InfoBrick.vue
@@ -0,0 +1,24 @@
+<template>
+  <panel elevated>
+    <div class="info-brick has-padding-1">
+      <div class="info-brick-title">
+        <h3
+          class="title is-4"
+          v-html="data.title"
+        ></h3>
+
+        <p
+          class="subtitle is-6 has-text-grey"
+          v-html="data.description"
+        ></p>
+      </div>
+    </div>
+  </panel>
+</template>
+
+<script>
+
+export default {
+  props: ['data']
+}
+</script>
